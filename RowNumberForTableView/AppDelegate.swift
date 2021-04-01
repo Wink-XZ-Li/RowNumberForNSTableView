dad4603991b9fee49d63b0992ff58e80029c9427
Wink <a318647551@163.com>
2021-04-01 08:25:55 +0800

    Initial Commit

---
+++ RowNumberForTableView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RowNumberForTableView
+//
+//  Created by Wink on 2021/4/1.
+//  Copyright © 2021 Wink. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RowNumberForTableView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RowNumberForTableView
+//
+//  Created by Wink on 2021/4/1.
+//  Copyright © 2021 Wink. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
